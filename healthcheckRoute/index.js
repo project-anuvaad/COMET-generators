25da5226a11a2710ec3b4b87c892344e3dab9342
hassanamin994 <hassanmamin994@gmail.com>
2020-07-13 23:44:28 +0200

    add healthcheckRoute generator

---
+++ healthcheckRoute/index.js
@@ -0,0 +1,19 @@
+module.exports = ({ router, mongoConnection, rabbitmqConnection }) => {
+
+    router.get('/health', (req, res) => {
+        if (mongoConnection) {
+            const { readyState } = mongoConnection;
+            // readyState is either 1 or to for connected or connecting
+            if (readyState !== 1 && readyState !== 2) {
+                return res.status(400).send('DATABASE CONNECTION DROPPED');
+            }
+        }
+        if (rabbitmqConnection) {
+            const { readable, writable } = rabbitmqConnection.stream;
+            if (!readable || !writable) {
+                return res.status(400).send('RABBITMQ CONNECTION DROPPED');
+            }
+        }
+        return res.status(200).send('OK');
+    })
+}
