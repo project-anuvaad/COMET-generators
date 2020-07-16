24fcf6b23091e13ffe4e2fe45be0b2f3d613ff66
hassanamin994 <hassanmamin994@gmail.com>
2020-07-16 04:20:11 +0200

    change status code to 503 for healtcheck

--- healthcheckRouteGenerator/index.js
+++ healthcheckRouteGenerator/index.js
@@ -12,13 +12,13 @@ module.exports = ({
       const { readyState } = mongoConnection;
       // readyState is either 1 or to for connected or connecting
       if (readyState !== 1 && readyState !== 2) {
-        return res.status(400).send("DATABASE CONNECTION DROPPED");
+        return res.status(503).send("DATABASE CONNECTION DROPPED");
       }
     }
     if (rabbitmqConnection) {
       const { readable, writable } = rabbitmqConnection.stream;
       if (!readable || !writable) {
-        return res.status(400).send("RABBITMQ CONNECTION DROPPED");
+        return res.status(503).send("RABBITMQ CONNECTION DROPPED");
       }
     }
     return res.status(200).send("OK");
