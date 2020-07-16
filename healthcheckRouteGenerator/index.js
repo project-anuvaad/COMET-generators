561bd1dad1bf2a724b9aae3368c866aa557311b3
hassanamin994 <hassanmamin994@gmail.com>
2020-07-16 21:16:47 +0200

    log healthcheck failure reason

--- healthcheckRouteGenerator/index.js
+++ healthcheckRouteGenerator/index.js
@@ -12,12 +12,14 @@ module.exports = ({
       const { readyState } = mongoConnection;
       // readyState is either 1 or to for connected or connecting
       if (readyState !== 1 && readyState !== 2) {
+        console.log('DATABASE CONNECTION DROPPED');
         return res.status(503).send("DATABASE CONNECTION DROPPED");
       }
     }
     if (rabbitmqConnection) {
       const { readable, writable } = rabbitmqConnection.stream;
       if (!readable || !writable) {
+        console.log('RABBITMQ CONNECTION DROPPED');
         return res.status(503).send("RABBITMQ CONNECTION DROPPED");
       }
     }
